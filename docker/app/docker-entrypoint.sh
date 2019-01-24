1266783f80c7b77a474b5ed76dd29fc6f5a54ab3
Juha Yrjölä <juha.yrjola@iki.fi>
2019-01-24 16:54:01 +0200

    Docker: Make gunicorn log requests to stdout

--- docker/app/docker-entrypoint.sh
+++ docker/app/docker-entrypoint.sh
@@ -4,4 +4,5 @@ set -e
 
 cd /code
 python manage.py migrate --no-input
-exec gunicorn --bind :8000 aplans.wsgi:application
+# Log to stdout
+exec gunicorn --access-logfile - --bind :8000 aplans.wsgi:application
