95b964236ebfc08f3d5da0e09afdff86a14e03ef
Juha Yrjölä <juha.yrjola@iki.fi>
2019-03-05 20:38:08 +0200

    Make Person.organization field autocompletable

--- people/admin.py
+++ people/admin.py
@@ -5,3 +5,4 @@ from .models import Person
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     search_fields = ('first_name', 'last_name',)
+    autocomplete_fields = ('organization',)
