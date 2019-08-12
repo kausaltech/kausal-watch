4de45e7c6678360ea7c2e0afe361ab1ee6020e70
Juha Yrjölä <juha.yrjola@iki.fi>
2019-08-12 10:12:24 +0300

    Hide user field from Person admin

--- people/admin.py
+++ people/admin.py
@@ -4,5 +4,6 @@ from .models import Person
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
+    fields = ('first_name', 'last_name', 'email', 'organization')
     search_fields = ('first_name', 'last_name',)
     autocomplete_fields = ('organization',)
