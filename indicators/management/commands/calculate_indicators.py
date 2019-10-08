1f6d7f8aba5d07c4c7dd19bf25f84d4df8c481f7
Juha Yrjölä <juha.yrjola@iki.fi>
2019-10-08 18:06:12 +0300

    Add management command to generate data for meta-indicators

---
+++ indicators/management/commands/calculate_indicators.py
@@ -0,0 +1,68 @@
+from datetime import timedelta
+import logging
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from indicators.models import Indicator, IndicatorValue
+
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = 'Recalculates meta-indicator values'
+
+    def handle_share_of_updated_indicators(self, indicator, plan):
+        now = timezone.now()
+        all_actions = list(plan.actions.all().exclude(status__is_completed=True))
+        if not all_actions:
+            return
+        up_to_date = 0
+        for action in all_actions:
+            if now - action.updated_at <= timedelta(days=3 * 30):
+                up_to_date += 1
+
+        share = round(up_to_date * 100 / len(all_actions), 1)
+        try:
+            val = indicator.values.get(date=now.date())
+        except IndicatorValue.DoesNotExist:
+            val = IndicatorValue(indicator=indicator, date=now.date())
+        val.value = share
+        val.save()
+
+    def handle_average_days_since_last_update(self, indicator, plan):
+        now = timezone.now()
+        all_actions = list(plan.actions.all().exclude(status__is_completed=True))
+        updated_actions = 0
+        days_since_update = 0
+        for action in all_actions:
+            if not action.updated_at:
+                continue
+            updated_actions += 1
+            days_since_update += (now - action.updated_at).days
+
+        if not updated_actions:
+            return
+
+        avg = round(days_since_update / updated_actions, 1)
+        try:
+            val = indicator.values.get(date=now.date())
+        except IndicatorValue.DoesNotExist:
+            val = IndicatorValue(indicator=indicator, date=now.date())
+        val.value = avg
+        val.save()
+
+    def handle(self, *args, **options):
+        IDENTIFIERS = ['share_of_updated_indicators', 'average_days_since_last_update']
+
+        for identifier in IDENTIFIERS:
+            indicators = Indicator.objects.filter(identifier=identifier)
+            for ind in indicators:
+                if ind.time_resolution != 'day':
+                    ind.time_resolution = 'day'
+                    ind.save(update_fields=['time_resolution'])
+                if ind.levels.count() != 1:
+                    logger.error('Indicator %s has %d active plans' % (str(ind), ind.levels.count()))
+                plan = ind.plans.first()
+                handler = getattr(self, 'handle_%s' % identifier)
+                handler(ind, plan)
