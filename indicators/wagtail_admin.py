84febe6dece49972fedfc54a776552f4cf629b38
Juha Yrjölä <juha.yrjola@iki.fi>
2020-08-09 23:38:35 +0300

    Add wagtail admin for Indicator

---
+++ indicators/wagtail_admin.py
@@ -0,0 +1,15 @@
+from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+from admin_site.wagtail import AdminOnlyPanel, AplansModelAdmin, AplansTabbedInterface
+
+from .models import Indicator
+
+
+class IndicatorAdmin(AplansModelAdmin):
+    model = Indicator
+    menu_icon = 'wagtail'  # change as required
+    menu_order = 300  # will put in 3rd place (000 being 1st, 100 2nd)
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+modeladmin_register(IndicatorAdmin)
