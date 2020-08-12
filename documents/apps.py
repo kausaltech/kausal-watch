3229fcdfd8ae89811501627b9607915fd3cdfa39
Juha Yrjölä <juha.yrjola@iki.fi>
2020-08-12 21:54:06 +0300

    Monkeypatch document collections

--- documents/apps.py
+++ documents/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class DocumentsConfig(AppConfig):
     name = 'documents'
+
+    def ready(self):
+        # monkeypatch filtering of Collections
+        from .chooser import monkeypatch_chooser
+        monkeypatch_chooser()
