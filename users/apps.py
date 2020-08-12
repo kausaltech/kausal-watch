9cc53d71872b7465aec6f649cfc9ffb9a1a71989
Juha Yrjölä <juha.yrjola@iki.fi>
2020-08-12 19:01:19 +0300

    Use Person avatar as default user profile picture

--- users/apps.py
+++ users/apps.py
@@ -9,5 +9,14 @@ class UsersConfig(AppConfig):
     def ready(self):
         from django.contrib.auth import user_logged_in
         from .perms import create_permissions
+        from wagtail.admin import wagtail_hooks  # noqa
+        from wagtail.core import hooks
 
         user_logged_in.connect(create_permissions)
+        account_hooks = hooks._hooks['register_account_menu_item']
+        for idx, val in enumerate(account_hooks):
+            if val[0] == wagtail_hooks.register_account_set_profile_picture:
+                break
+        else:
+            return
+        account_hooks.remove(val)
