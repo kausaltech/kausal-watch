85fb12bd9f6e95c715c51f7269a4d1e6ce5b3c12
Juha Yrjölä <juha.yrjola@iki.fi>
2020-08-15 15:56:22 +0300

    Improve editing of the other_languages field

--- aplans/utils.py
+++ aplans/utils.py
@@ -1,8 +1,10 @@
 from typing import Iterable, List
 import re
+from django import forms
 from django.db import models
 from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
+from django.contrib.postgres.fields import ArrayField
 
 
 def camelcase_to_underscore(name):
@@ -68,3 +70,23 @@ class OrderedModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class ChoiceArrayField(ArrayField):
+    """
+    A field that allows us to store an array of choices.
+
+    Uses Django 1.9's postgres ArrayField
+    and a MultipleChoiceField for its formfield.
+    """
+
+    def formfield(self, **kwargs):
+        defaults = {
+            'form_class': forms.MultipleChoiceField,
+            'choices': self.base_field.choices,
+        }
+        defaults.update(kwargs)
+        # Skip our parent's formfield implementation completely as we don't
+        # care for it.
+        # pylint:disable=bad-super-call
+        return super(ArrayField, self).formfield(**defaults)
