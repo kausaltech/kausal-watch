cb20f4061c82ffe7b51c028ec00d39e68c795f36
Juha Yrjölä <juha.yrjola@iki.fi>
2020-09-01 17:16:57 +0300

    Set the user locale from wagtail profile or default to the Plan primary language

--- aplans/middleware.py
+++ aplans/middleware.py
@@ -1,12 +1,13 @@
-
 import sentry_sdk
-from django.urls import reverse
 from django.conf import settings
 from django.contrib import messages
 from django.shortcuts import redirect
-from django.utils.translation import ugettext_lazy as _
+from django.urls import reverse
 from django.utils.deprecation import MiddlewareMixin
+from django.utils.translation import activate
+from django.utils.translation import ugettext_lazy as _
 from social_core.exceptions import SocialAuthBaseException
+from wagtail.users.models import UserProfile
 
 
 class SocialAuthExceptionMiddleware(MiddlewareMixin):
@@ -31,9 +32,19 @@ class SocialAuthExceptionMiddleware(MiddlewareMixin):
 
 class AdminMiddleware(MiddlewareMixin):
     def process_view(self, request, *args, **kwargs):
-        if not request.user or not request.user.is_authenticated or not request.user.is_staff:
+        user = request.user
+        if not user or not user.is_authenticated or not user.is_staff:
             return
+
+        profile = UserProfile.get_for_user(user)
         plan = request.user.get_active_admin_plan()
+
+        if profile.preferred_language and profile.preferred_language in (x[0] for x in settings.LANGUAGES):
+            activate(profile.preferred_language)
+        else:
+            profile.preferred_language = plan.primary_language
+            profile.save(update_fields=['preferred_language'])
+
         if not plan.site_id:
             return
         request._wagtail_site = plan.site
