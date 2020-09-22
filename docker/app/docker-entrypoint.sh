ce99647c721d2fdd4b560f1c56f2642ab7cff9d7
Juha Yrjölä <juha.yrjola@iki.fi>
2020-09-22 11:55:31 +0300

    Enable threads in uWSGI to make Sentry work better

--- docker/app/docker-entrypoint.sh
+++ docker/app/docker-entrypoint.sh
@@ -8,6 +8,7 @@ cd /code
 python manage.py migrate --no-input
 # Log to stdout
 exec uwsgi --http-socket :8000 --socket :8001 --processes 4 \
+    --enable-threads \
     --buffer-size=32768 \
     --static-map /static=/srv/static \
     --static-map /media=/srv/media \
