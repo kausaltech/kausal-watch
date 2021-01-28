9ebe9ffcf506bdf43119e686ea750492d5b7f591
Bernhard Bliem <bernhard@bliem.xyz>
2021-01-28 15:43:51 +0200

    Add unit test for invalid colors

--- actions/tests/test_models.py
+++ actions/tests/test_models.py
@@ -2,7 +2,7 @@ import pytest
 from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
 
-from actions.models import Action
+from actions.models import Action, Category, CategoryType
 
 
 @pytest.mark.django_db
@@ -20,3 +20,10 @@ def test_action_no_duplicate_identifier_per_plan(plan):
     Action.objects.create(plan=plan, name='Test action 1', identifier='id')
     with pytest.raises(IntegrityError):
         Action.objects.create(plan=plan, name='Test action 2', identifier='id')
+
+
+@pytest.mark.django_db
+def test_category_color_invalid(category):
+    category.color = 'invalid'
+    with pytest.raises(ValidationError):
+        category.full_clean()
