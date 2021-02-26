058a709f03b96aa270ce573fe7bd518c87f76b86
Bernhard Bliem <bernhard@bliem.xyz>
2021-02-26 20:19:26 +0200

    Update FIXME

--- actions/tests/factories.py
+++ actions/tests/factories.py
@@ -148,8 +148,8 @@ class ActionFactory(DjangoModelFactory):
                 self.responsible_parties.add(responsible_party)
 
 
-# FIXME: The factory name does not correspond to the model name because we also want a Person factory whose name
-# should arguably by ActionContactPerson. We might want to consider renaming the model ActionContactPerson to
+# FIXME: The factory name does not correspond to the model name because we also want a Person fixture whose name
+# should arguably by action_contact_person. We might want to consider renaming the model ActionContactPerson to
 # ActionContact or similar.
 class ActionContactFactory(DjangoModelFactory):
     class Meta:
