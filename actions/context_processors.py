f15b44c0e18aa98da53f2ca106ea5d85d0571e1d
Juha Yrjölä <juha.yrjola@iki.fi>
2021-03-19 14:54:09 +0200

    Speed up context processor by caching results

--- actions/context_processors.py
+++ actions/context_processors.py
@@ -4,6 +4,17 @@ def current_plan(request):
     out = {}
     if not request or not request.user or not request.user.is_authenticated:
         return out
-    out['active_plan'] = request.user.get_active_admin_plan()
-    out['active_client'] = Client.objects.for_request(request).first()
+    if getattr(request, '_active_plan', None):
+        plan = request._active_plan
+    else:
+        plan = request.user.get_active_admin_plan()
+        request._active_plan = plan
+    if getattr(request, '_active_client', None):
+        client = request._active_client
+    else:
+        client = Client.objects.for_request(request).first()
+        request._active_client = client
+
+    out['active_plan'] = plan
+    out['active_client'] = client
     return out
