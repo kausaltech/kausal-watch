e787f61004553a8a079cec1bc2e7488dc63f2ac4
Bernhard Bliem <bernhard@bliem.xyz>
2021-04-06 09:42:59 +0300

    Fix error on creating new pages

--- pages/wagtail_admin.py
+++ pages/wagtail_admin.py
@@ -11,5 +11,5 @@ def remove_page_listing_more_button(buttons, page, page_perms, is_parent=False, 
 
 @hooks.register('construct_page_action_menu')
 def remove_page_action_menu_items_except_publish(menu_items, request, context):
-    if getattr(context['page'], 'remove_page_action_menu_items_except_publish', False):
+    if getattr(context.get('page'), 'remove_page_action_menu_items_except_publish', False):
         menu_items[:] = [i for i in menu_items if i.__class__.__name__ == 'PublishMenuItem']
