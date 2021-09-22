d4bb5fdd3289c760c38a41df4ac94065fa7d7f8c
Bernhard Bliem <bernhard@bliem.xyz>
2021-09-22 18:05:29 +0200

    Create categories for different types of actions

--- kpr_import_from_json.py
+++ kpr_import_from_json.py
@@ -15,7 +15,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aplans.settings')
 django.setup()
 
 from django_orghierarchy.models import DataSource, Organization  # noqa
-from actions.models import Action, ActionImplementationPhase, ActionLink, ActionResponsibleParty, Category, CategoryMetadataNumericValue, CategoryType, CategoryTypeMetadata, Plan  # noqa
+from actions.models import Action, ActionImplementationPhase, ActionLink, ActionResponsibleParty, Category, CategoryIcon, CategoryMetadataNumericValue, CategoryType, CategoryTypeMetadata, Plan  # noqa
 from indicators.models import Indicator, IndicatorLevel, IndicatorGoal, IndicatorValue, Unit  # noqa
 from images.models import AplansImage  # noqa
 from pages.models import CategoryPage  # noqa
@@ -85,24 +85,69 @@ plan, _ = Plan.objects.update_or_create(
 )
 
 # Create CategoryType for transitions
-category_type_data = {
+transitions_category_type_data = {
     'name': "Omställningar",
     'usable_for_actions': True,
     'hide_category_identifiers': True,
 }
-category_type, _ = CategoryType.objects.update_or_create(
+transitions_category_type, _ = CategoryType.objects.update_or_create(
     plan=plan,
     identifier='transition',
-    defaults=category_type_data,
+    defaults=transitions_category_type_data,
 )
 metadata, _ = CategoryTypeMetadata.objects.update_or_create(
-    type=category_type,
+    type=transitions_category_type,
     identifier='impact',
     defaults=dict(
         name='Utsläppsminskning',
         format=CategoryTypeMetadata.MetadataFormat.NUMERIC
     )
 )
+
+# Create CategoryType whose catogies will be the different types of actions
+action_types_category_type_data = {
+    'name': "Typ",
+    'usable_for_actions': True,
+    'hide_category_identifiers': True,
+}
+action_types_category_type, _ = CategoryType.objects.update_or_create(
+    plan=plan,
+    identifier='action',
+    defaults=action_types_category_type_data,
+)
+
+action_types = {
+    'administration': 'Administrativ åtgärd',
+    'commitment': 'Åtagande',
+    'financial': 'Ekonomisk åtgärd',
+    'information': 'Informativ åtgär',
+    'policy': 'Styrmedel',
+    'research-development': 'Forskning och utveckling',
+}
+
+# Create action type categories and fetch icons
+for identifier, name in action_types.items():
+    category, _ = Category.objects.update_or_create(
+        type=action_types_category_type,
+        identifier=identifier,
+        defaults={
+            'name': name,
+        },
+    )
+
+    url = f'https://app.climateview.global/assets/images/action-types/{identifier}.svg'
+    response = requests.get(url)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        print(f"Could not download {url}: {response}")
+    else:
+        CategoryIcon.objects.update_or_create(
+            category=category,
+            defaults={
+                'data': response.content,
+            },
+        )
 
 
 def import_image(url, credit=''):
@@ -303,7 +348,7 @@ for i, indicator_data in enumerate(indicators.values()):
 
 # Create a root category for total emissions
 root_category, _ = Category.objects.update_or_create(
-    type=category_type,
+    type=transitions_category_type,
     identifier='root',
     defaults={
         'name': 'Totala utsläpp',
@@ -327,7 +372,7 @@ for i, node in enumerate(nodes.values()):
         'color': SECTOR_COLORS[node['title']] if parent is None or parent is root_category else None
     }
     category, _ = Category.objects.update_or_create(
-        type=category_type,
+        type=transitions_category_type,
         identifier=str(i+1),
         defaults=category_data,
     )
