7339dabb1eff9469503e3bce4ba1621f4dc81818
Bernhard Bliem <bernhard@bliem.xyz>
2021-09-29 22:57:42 +0200

    Add tests

--- actions/tests/test_models.py
+++ actions/tests/test_models.py
@@ -4,12 +4,62 @@ from django.db.utils import IntegrityError
 
 from actions.models import Action
 from actions.tests.factories import ActionFactory, CategoryFactory
+from orgs.tests.factories import OrganizationFactory
 
 pytestmark = pytest.mark.django_db
 
 
 def test_plan_can_be_saved(plan):
     pass
+
+
+def test_plan_get_related_organizations(plan):
+    assert plan.organization
+    org = OrganizationFactory()
+    sub_org1 = OrganizationFactory(parent=plan.organization)
+    sub_org2 = OrganizationFactory(parent=org)
+    result = list(plan.get_related_organizations())
+    assert result == [plan.organization, sub_org1]
+    plan.related_organizations.add(org)
+    result = list(plan.get_related_organizations())
+    assert result == [plan.organization, sub_org1, org, sub_org2]
+
+
+def test_action_query_set_modifiable_by_not_modifiable(action, user):
+    assert action not in Action.objects.modifiable_by(user)
+
+
+def test_action_query_set_modifiable_by_superuser(action, superuser):
+    assert action in Action.objects.modifiable_by(superuser)
+
+
+def test_action_query_set_modifiable_by_plan_admin(action, plan_admin_user):
+    assert action in Action.objects.modifiable_by(plan_admin_user)
+
+
+def test_action_query_set_modifiable_by_contact_person(action, action_contact):
+    assert action in Action.objects.modifiable_by(action_contact.person.user)
+
+
+def test_action_query_set_modifiable_by_organization_admin_not_responsible(action, organization_admin):
+    assert action not in Action.objects.modifiable_by(organization_admin.user)
+
+
+def test_action_query_set_modifiable_by_organization_admin_responsible(
+    action, organization_admin, action_responsible_party
+):
+    assert action in Action.objects.modifiable_by(organization_admin.user)
+
+
+def test_action_query_set_modifiable_by_distinct(action, user, action_contact):
+    assert user == action_contact.person.user
+    assert list(Action.objects.modifiable_by(user)) == [action]
+    # Make user a plan admin as well
+    user.general_admin_plans.add(action.plan)
+    assert list(Action.objects.modifiable_by(user)) == [action]
+    # When we remove the user from the action contacts, it should still be able to modify
+    action_contact.delete()
+    assert list(Action.objects.modifiable_by(user)) == [action]
 
 
 def test_action_can_be_saved():
