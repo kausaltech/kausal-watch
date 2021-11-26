359867b8db2c9abd26cb0fc5ef40322fb2a20680
Bernhard Bliem <bernhard@bliem.xyz>
2021-11-26 08:58:51 +0100

    Hide completion date when state is not 'completed'

---
+++ actions/static/actions/action-tasks-wagtail.js
@@ -0,0 +1,26 @@
+// Whenever the state of an action task is changed, show or hide the 'completed at' field
+const selectSelector = 'select[id^="id_tasks-"][id$="-state"]';
+function changeCompletedAt() {
+    $(selectSelector).each(function () {
+        const state = $(this).val();
+        const $card = $(this).closest('div[class~="condensed-inline-panel__card"]')
+        const $completedAt = $card.find('input[id^="id_tasks-"][id$="-completed_at"]');
+        const $completedAtListItem = $completedAt.closest('li');
+        if (state === 'completed') {
+            $completedAtListItem.show();
+        } else {
+            $completedAtListItem.hide();
+            $completedAt.val('');
+        }
+    });
+}
+
+$(document).on("change", selectSelector, changeCompletedAt);
+
+// The same after creating the form
+const editButtonSelector = 'li[class~="condensed-inline-panel__action-edit"]'
+$(document).on('click', editButtonSelector, function (ev) {
+    setTimeout(function () {
+        changeCompletedAt();
+    }, 0);
+});
--- actions/wagtail_hooks.py
+++ actions/wagtail_hooks.py
@@ -1,6 +1,8 @@
-from . import wagtail_admin  # noqa
+from django.templatetags.static import static
 from wagtail.core import hooks
 from wagtail.admin.site_summary import SummaryItem
+
+from . import wagtail_admin  # noqa
 
 class ActionsSummaryItem(SummaryItem):
     order = 200
@@ -20,3 +22,8 @@ class ActionsSummaryItem(SummaryItem):
 @hooks.register('construct_homepage_summary_items')
 def add_actions_summary_item(request, items):
     items.append(ActionsSummaryItem(request))
+
+
+@hooks.register('insert_editor_js')
+def editor_js():
+    return f'<script src="{static("actions/action-tasks-wagtail.js")}"></script>'
