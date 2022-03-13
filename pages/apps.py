24709d6576b1d8ab182bb6313ab4df16ab8b853f
Bernhard Bliem <bernhard@bliem.xyz>
2022-03-13 18:04:26 +0100

    Monkey-patch explorable pages and site for user

--- pages/apps.py
+++ pages/apps.py
@@ -1,38 +1,40 @@
 from django.apps import AppConfig
-from django.db.models import Q
+from django.conf import settings
 
 
-_wagtail_get_pages_with_direct_explore_permission = None
-
-
-def filter_plan_pages(user):
+def get_pages_with_direct_explore_permission(user):
     from wagtail.core.models import Page
+    return Page.objects.filter(depth=1)
 
-    pages = _wagtail_get_pages_with_direct_explore_permission(user)
-    if user.is_superuser:
-        pages = Page.objects.all()
 
+def get_site_for_user(user):
     plan = user.get_active_admin_plan()
-    if plan.root_page:
-        # pages = pages.descendant_of(plan.root_page, inclusive=True)
-        q = pages.descendant_of_q(plan.root_page, inclusive=True)
-        for translation in plan.root_page.get_translations():
-            q |= pages.descendant_of_q(translation, inclusive=True)
-        # Do not exclude global root page (i.e., parent of plan.root_page), otherwise language switcher won't work
-        q |= Q(depth=1)
-        pages = pages.filter(q)
+    if not plan:
+        return None
 
-    return pages
+    root_page = plan.root_page
+    if root_page:
+        root_site = root_page.get_site()
+    else:
+        root_site = None
+
+    real_site_name = None
+    if root_site:
+        real_site_name = root_site.site_name if root_site.site_name else root_site.hostname
+    return {
+        'root_page': root_page,
+        'root_site': root_site,
+        'site_name': real_site_name if real_site_name else settings.WAGTAIL_SITE_NAME,
+    }
 
 
 def patch_wagtail_page_hierarchy():
     """Monkeypatch filtering pages by the currently active action plan."""
     from wagtail.admin import navigation
-
-    global _wagtail_get_pages_with_direct_explore_permission
-
-    _wagtail_get_pages_with_direct_explore_permission = navigation.get_pages_with_direct_explore_permission
-    navigation.get_pages_with_direct_explore_permission = filter_plan_pages
+    navigation.get_pages_with_direct_explore_permission = get_pages_with_direct_explore_permission
+    # The original get_site_for_user function determines site by explorable root page. Since here the explorable root
+    # page is always the global root this doesn't make much sense.
+    navigation.get_site_for_user = get_site_for_user
 
 
 def resolve_page_url_path(self, info):
