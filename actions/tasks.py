33bb53814745887c89966a94f153d94e81d5d411
Bernhard Bliem <bernhard@bliem.xyz>
2022-04-24 12:33:57 +0200

    Fix update_index task not being discovered

--- actions/tasks.py
+++ actions/tasks.py
@@ -5,3 +5,9 @@ from django.core import management
 @shared_task
 def update_action_status():
     management.call_command('update_action_status')
+
+
+@shared_task
+def update_index():
+    # Actually this is not specific to the `actions` app, so maybe should be in a different file
+    management.call_command('update_index')
--- aplans/tasks.py
+++
@@ -1,7 +0,0 @@
-from celery import shared_task
-from django.core import management
-
-
-@shared_task
-def update_index():
-    management.call_command('update_index')
