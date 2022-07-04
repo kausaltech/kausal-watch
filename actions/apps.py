8ae2505adcbdf3e5d8634a3d68808a2d20f7b1cc
Bernhard Bliem <bernhard@bliem.xyz>
2022-07-04 12:02:43 +0200

    Filter SVGs in chooser by collection

--- actions/apps.py
+++ actions/apps.py
@@ -8,7 +8,33 @@ from django.utils.translation import gettext_lazy as _
 collections.Iterable = collections.abc.Iterable
 collections.Mapping = collections.abc.Mapping
 
+_wagtailsvg_get_unfiltered_object_list = None
+
+
+def get_unfiltered_object_list(self):
+    plan = self.request.user.get_active_admin_plan()
+    if plan.root_collection is not None:
+        collections = plan.root_collection.get_descendants(inclusive=True)
+    else:
+        collections = []
+    return self.model.objects.filter(collection__in=collections)
+
+
+def monkeypatch_svg_chooser():
+    from wagtailsvg.views import SvgModelChooserMixin
+    global _wagtailsvg_get_unfiltered_object_list
+
+    if _wagtailsvg_get_unfiltered_object_list is not None:
+        return
+
+    _wagtailsvg_get_unfiltered_object_list = SvgModelChooserMixin.get_unfiltered_object_list
+    SvgModelChooserMixin.get_unfiltered_object_list = get_unfiltered_object_list
+
 
 class ActionsConfig(AppConfig):
     name = 'actions'
     verbose_name = _('Actions')
+
+    def ready(self):
+        # monkeypatch filtering of Collections
+        monkeypatch_svg_chooser()
