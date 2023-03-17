8302f0dceee01a16590400e9968c8a3a0f16a7c8
Timo Tuominen <tituomin@gmail.com>
2023-03-17 11:21:25 +0200

    Change admin UI widget to fit longer words
    Radio buttons do not scale with languages

--- actions/action_admin.py
+++ actions/action_admin.py
@@ -506,7 +506,7 @@ class ActionAdmin(OrderableMixin, AplansModelAdmin):
             )
         ),
         FieldPanel('merged_with', widget=ActionChooser),
-        FieldPanel('visibility', widget=forms.RadioSelect()),
+        FieldPanel('visibility'),
     ]
 
     progress_panels = [
