7a52f27d174ffe62946df18aa23d50aef6af31b2
Bernhard Bliem <bernhard@bliem.xyz>
2023-03-22 13:31:26 +0100

    Return None in resolve_values_for_action when there is no snapshot

--- reports/schema.py
+++ reports/schema.py
@@ -6,12 +6,13 @@ from aplans.graphql_types import DjangoNode, register_django_node
 from aplans.utils import public_fields
 from actions.models import Action
 from reports.blocks.action_content import ReportFieldBlock, ReportValueInterface
-from reports.models import Report, ReportType
+from reports.models import ActionSnapshot, Report, ReportType
 
 
 @register_django_node
 class ReportNode(DjangoNode):
     fields = graphene.List(graphene.NonNull(lambda: grapple_registry.streamfield_blocks.get(ReportFieldBlock)))
+    # values_for_action is null if there is no snapshot for the specified action and the report
     values_for_action = graphene.List(
         graphene.NonNull(ReportValueInterface),
         # Either action_id or action_identifier must be specified
@@ -31,7 +32,10 @@ class ReportNode(DjangoNode):
             action = plan_actions.get(id=action_id)
         else:
             action = plan_actions.get(identifier=action_identifier)
-        snapshot = action.get_latest_snapshot(root)
+        try:
+            snapshot = action.get_latest_snapshot(root)
+        except ActionSnapshot.DoesNotExist:
+            return None
         return [field.block.graphql_value_for_action_snapshot(field, snapshot) for field in root.fields]
 
 
