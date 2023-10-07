b096e5c2c9772c862303a008e2b0f66e39efff1e
Bernhard Bliem <bernhard@bliem.xyz>
2023-10-07 13:01:36 +0200

    Allow SVG files for Wagtail images

--- aplans/settings.py
+++ aplans/settings.py
@@ -511,6 +511,7 @@ CKEDITOR_CONFIGS = {
 
 WAGTAILDOCS_DOCUMENT_MODEL = 'documents.AplansDocument'
 WAGTAILIMAGES_IMAGE_MODEL = 'images.AplansImage'
+WAGTAILIMAGES_EXTENSIONS = ['gif', 'jpg', 'jpeg', 'png', 'webp', 'svg']
 WAGTAILEMBEDS_FINDERS = [
     {
         'class': 'wagtail.embeds.finders.oembed'
