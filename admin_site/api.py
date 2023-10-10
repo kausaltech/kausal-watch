2318e3ba4bbd62f7eb2c80c9588fbd8cc62c98cc
Bernhard Bliem <bernhard@bliem.xyz>
2023-10-10 17:11:03 +0200

    Call User.can_access_admin instead of create_permissions
    Also print more informative error messages.

--- admin_site/api.py
+++ admin_site/api.py
@@ -24,35 +24,41 @@ def check_login_method(request):
     d = request.data
     if not d or not isinstance(d, dict):
         msg = _("Invalid email address")
-        raise ValidationError(dict(detail=msg, code="invalid_email"))
+        raise ValidationError({'detail': msg, 'code': 'invalid_email'})
 
     email = d.get('email', '').strip().lower()
     if not email:
         msg = _("Invalid email address")
-        raise ValidationError(dict(detail=msg, code="invalid_email"))
+        raise ValidationError({'detail': msg, 'code': 'invalid_email'})
     user = User.objects.filter(email__iexact=email).first()
     person = user.get_corresponding_person() if user else None
     if user is None or person is None:
         msg = _("No user found with this email address. Ask your administrator to create an account for you.")
-        raise ValidationError(dict(detail=msg, code="no_user"))
+        raise ValidationError({'detail': msg, 'code': 'no_user'})
 
-    # Before we check for permissions, we may need to create them first
-    # TODO: Don't create permissions here; instead use User.can_access_admin()
-    create_permissions(user)
+    if not user.can_access_admin():
+        msg = _(
+            "You do not have admin access. Your administrator may need to assign you an action or indicator, or grant "
+            "you plan admin status."
+        )
+        raise ValidationError({'detail': msg, 'code': 'no_admin_access'})
+
+    # Always use password authentication if the user has a password
+    if user.has_usable_password():
+        return Response({'method': 'password'})
+
+    # Use the client's authorization backend
     try:
         client = person.get_admin_client()
     except:
         client = None
 
-    msg = _("This user does not have access to admin.")
-    error = ValidationError(dict(detail=msg, code="no_admin_access"))
+    if client is None:
+        msg = _("Cannot determine authorization method. The email address domain may be unknown.")
+        raise ValidationError({'detail': msg, 'code': 'no_client'})
 
-    if not user.has_perm('wagtailadmin.access_admin'):
-        # TODO: distinguish this case to the user, indicating the login was succesful but the plan admins need to assign responsibilities to
-        # the person
-        raise error
-    if user.has_usable_password():
-        return Response({'method': 'password'})
-    if client is None or not client.auth_backend:
-        raise error
+    if not client.auth_backend:
+        msg = _("Password authentication is required, but the user has no password.")
+        raise ValidationError({'detail': msg, 'code': 'no_password'})
+
     return Response({'method': client.auth_backend})
