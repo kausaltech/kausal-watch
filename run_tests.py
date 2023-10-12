65fbc4d0ed114d01b84face4b1c61bf8ea763356
Juha Yrjölä <juha.yrjola@iki.fi>
2023-10-12 13:39:54 +0300

    Add the '--pyargs' argument to pytest in run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -7,7 +7,7 @@ import pytest
 EXTENSIONS_PACKAGE_NAME = 'kausal_watch_extensions'
 
 if __name__ == '__main__':
-    args = [*sys.argv[1:], '.']
+    args = ['--pyargs', *sys.argv[1:], '.']
     if importlib.util.find_spec(EXTENSIONS_PACKAGE_NAME) is not None:
         args.append(EXTENSIONS_PACKAGE_NAME)
     raise SystemExit(pytest.main(args))
