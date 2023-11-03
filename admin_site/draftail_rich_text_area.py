1da93ef4269d504497799510349a57dd2d340220
Juha Yrjölä <juha.yrjola@iki.fi>
2023-11-03 10:50:17 +0200

    Attempt a hotfix for the rich text language problems

--- admin_site/draftail_rich_text_area.py
+++ admin_site/draftail_rich_text_area.py
@@ -6,18 +6,37 @@ from wagtail.admin.rich_text import DraftailRichTextArea
 # Workaround until https://github.com/wagtail/wagtail/pull/11075 is merged
 class DraftailRichTextAreaWithFixedTranslations(DraftailRichTextArea):
     def get_context(self, name, value, attrs):
+        type_to_trans_map = {
+            "LINK": gettext("Link"),
+            "DOCUMENT": gettext("Document"),
+            "EMBED": gettext("Embed"),
+            "IMAGE": gettext("Image"),
+            "BOLD": gettext("Bold"),
+            "ITALIC": gettext("Italic"),
+            "SUPERSCRIPT": gettext("Superscript"),
+            "SUBSCRIPT": gettext("Subscript"),
+            "header-two": gettext("Heading %(level)d") % {"level": 2},
+            "header-three": gettext("Heading %(level)d") % {"level": 3},
+            "header-four": gettext("Heading %(level)d") % {"level": 4},
+            "ordered-list-item": gettext("Numbered list"),
+            "unordered-list-item": gettext("Bulleted list"),
+        }
+
         old_options = self.options
         self.options = deepcopy(old_options)
         for option in self.options.values():
-            if isinstance(option, list):
-                for item in option:
-                    if isinstance(item, dict) and 'description' in item and isinstance(item['description'], str):
-                        # Use gettext on the description; but if it's "Heading <int>", we need to do some extra work
-                        match = re.fullmatch(r'Heading (\d+)', item['description'])
-                        if match:
-                            item['description'] = gettext('Heading %(level)d') % {'level': int(match.group(1))}
-                        else:
-                            item['description'] = gettext(item['description'])
+            if not isinstance(option, list):
+                continue
+            for item in option:
+                if not isinstance(item, dict):
+                    continue
+                item_type = item.get('type')
+                if not item_type:
+                    continue
+                new_trans = type_to_trans_map.get(item_type)
+                if not new_trans:
+                    continue
+                item['description'] = new_trans
         context = super().get_context(name, value, attrs)
         self.options = old_options
         return context
