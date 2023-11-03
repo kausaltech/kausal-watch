deea91d670cad68296ce802e3708baf3e6280f17
Juha Yrjölä <juha.yrjola@iki.fi>
2023-11-03 15:41:49 +0200

    Perform Django system checks when starting under uWSGI
    This way the runtime state is closer to what we have in development,
    and we save a bit of memory by loading more code into memory before
    uWSGI forks the workers.

--- aplans/wsgi.py
+++ aplans/wsgi.py
@@ -8,9 +8,35 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+from loguru import logger
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aplans.settings')
 
 application = get_wsgi_application()
+
+def run_deployment_checks():
+    from django.core import checks  # noqa
+
+    msgs: list[checks.CheckMessage] = checks.run_checks(include_deployment_checks=True)
+    LEVEL_MAP = {
+        checks.DEBUG: 'DEBUG',
+        checks.INFO: 'INFO',
+        checks.WARNING: 'WARNING',
+        checks.ERROR: 'ERROR',
+        checks.CRITICAL: 'CRITICAL',
+    }
+
+    for msg in msgs:
+        msg.hint = None
+        logger.log(LEVEL_MAP.get(msg.level, 'WARNING'), str(msg))
+
+# We execute all the checks when running under uWSGI, so that we:
+#   - load more of the code to save memory after uWSGI forks workers
+#   - keep the state of the system closer to how it is under runserver
+try:
+    import uwsgi  # type: ignore  # noqa
+    run_deployment_checks()
+except ImportError:
+    pass
