36bf1ca4e987073387ea8794bd712c23f14df42f
Timo Tuominen <tituomin@gmail.com>
2024-02-01 14:33:01 +0200

    Add new simple email sender class
    The purpose of this is simply to:
    1) reuse the same connection for sending several emails
    2) make sure the From: and Reply-To: headers are overridden for all
    emails that are sent (if desired)

---
+++ aplans/email_sender.py
@@ -0,0 +1,24 @@
+from django.core.mail import EmailMessage, get_connection
+
+
+class EmailSender:
+    messages: list[EmailMessage]
+    from_email: str | None
+    reply_to: list | None
+
+    def __init__(self, from_email: str | None=None, reply_to: list | None=None):
+        self.messages = []
+        self.from_email = from_email
+        self.reply_to = reply_to
+
+    def queue(self, msg):
+        if self.from_email:
+            msg.from_email = self.from_email
+        if self.reply_to:
+            msg.reply_to = self.reply_to
+        self.messages.append(msg)
+
+    def send_all(self) -> int:
+        with get_connection() as connection:
+            num_sent = connection.send_messages(self.messages)
+            return num_sent
