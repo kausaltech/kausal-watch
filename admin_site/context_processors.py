7e367cec7cc87d6c9401db78f0b5b27fb975527e
Juha Yrjölä <juha.yrjola@iki.fi>
2024-02-28 14:37:40 +0200

    Sentry session replay for watch admin
    Load the browser Sentry when rendering the 500 error view
    
    This is useful in being able to correlate recorded sessions
    to backend error events.
    
    Add missing file
    
    Improve Sentry error reporting
    
    - Add a user feedback modal for 500 error page
    - Initialize the browser-side Sentry with correct options
      and also initialize browser Sentry on the internal 500
      page to be able to send session replays.
    
    sentrysdk updated

--- admin_site/context_processors.py
+++ admin_site/context_processors.py
@@ -1,9 +1,18 @@
 import json
 from django.conf import settings
+from django.utils.safestring import mark_safe
+from sentry_sdk import Hub
 
 
 def sentry(request):
-    return dict(sentry_dsn=settings.SENTRY_DSN, deployment_type=settings.DEPLOYMENT_TYPE)
+    hub: Hub = Hub.current
+    if not settings.SENTRY_DSN:
+        return {}
+    return dict(
+        sentry_dsn=settings.SENTRY_DSN, deployment_type=settings.DEPLOYMENT_TYPE,
+        sentry_trace_meta=mark_safe(hub.trace_propagation_meta()),
+        sentry_release=hub.client.options.get('release'),
+    )
 
 
 def i18n(request):
--- aplans/error_handling.py
+++ aplans/error_handling.py
@@ -1,5 +1,42 @@
-from django.views.defaults import server_error as html_server_error
+from django.http import HttpResponseServerError
+from django.views.defaults import ERROR_500_TEMPLATE_NAME
+from django.conf import settings
 from rest_framework.exceptions import server_error as json_server_error
+from django.views.decorators.csrf import requires_csrf_token
+from django.template import loader
+import sentry_sdk
+
+from actions.context_processors import current_plan
+from admin_site.context_processors import sentry
+
+
+@requires_csrf_token
+def html_server_error(request, template_name=ERROR_500_TEMPLATE_NAME):
+    """
+    500 error handler.
+
+    Templates: :template:`500.html`
+    Context: None
+    """
+    context = {}
+    try:
+        ret = current_plan(request)
+        if ret:
+            context.update(ret)
+    except Exception:
+        pass
+    try:
+        ret = sentry(request)
+        if ret:
+            context.update(ret)
+    except Exception:
+        pass
+
+    context['sentry_error_id'] = None
+    if settings.SENTRY_DSN:
+        context['sentry_error_id'] = sentry_sdk.last_event_id()
+    template = loader.get_template(template_name)
+    return HttpResponseServerError(template.render(context=context, request=request))
 
 
 def server_error(request, *args, **kwargs):
