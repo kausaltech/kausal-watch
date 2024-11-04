1cc88fa37e78959f5bf981320107c531fcbdec63
Timo Tuominen <tituomin@gmail.com>
2024-11-04 16:34:47 +0200

    Linting fixes

--- actions/blocks/mixins.py
+++ actions/blocks/mixins.py
@@ -1,13 +1,13 @@
+from __future__ import annotations
+
+
 class ActionListPageBlockPresenceMixin:
     def contains_model_instance(self, instance, blocks):
         container_block_name = self.model_instance_container_blocks[instance._meta.model]
         container_blocks = (child for child in blocks if child.block_type == container_block_name)
         child_block_class = self.child_blocks[container_block_name]
         subblock_name = child_block_class.model_instance_container_blocks[instance._meta.model]
-        for child in container_blocks:
-            if child.value.get(subblock_name) == instance:
-                return True
-        return False
+        return any(child.value.get(subblock_name) == instance for child in container_blocks)
 
     def insert_model_instance(self, instance, blocks):
         block_name = self.model_instance_container_blocks[instance._meta.model]
@@ -23,5 +23,6 @@ class ActionListPageBlockPresenceMixin:
             if (block.block_type == block_name and block.value[subblock_name] == instance):
                 break
         else:
-            raise ValueError(f"Model instance {instance} is not referenced in blocks")
+            msg = f'Model instance {instance} is not referenced in blocks'
+            raise ValueError(msg)
         blocks.pop(i)
