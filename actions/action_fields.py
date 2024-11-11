0ad223b92bb3d03a5e36dbf36e45c2ac74ff8dee
Timo Tuominen <tituomin@gmail.com>
2024-11-11 20:41:26 +0200

    Better implementation of disabled fields

--- actions/action_fields.py
+++ actions/action_fields.py
@@ -24,50 +24,65 @@ from .models.action import Action
 
 action_registry = ModelFieldRegistry(Action)
 
-def register(**kwargs):
+def register(field_name, **kwargs):
     action_registry.register(
-        ModelFieldProperties(**kwargs, model=Action),
+        ModelFieldProperties(field_name=field_name, **kwargs, model=Action),
     )
-    action_registry.update_with_defaults()
 
+# The following fields will have no blocks created for them whatsoever
+action_registry.disable_fields(
+    'completion',
+    'date_format',
+    'decision_level',
+    'dependency_role',
+    'dependent_relationships',
+    'id',
+    'impact',
+    'impact_groups',
+    'indicators',
+    'merged_with',
+    'monitoring_quality_points',
+    'order',
+    'plan',
+    'schedule_continuous',
+    'status_updates',
+    'superseded_actions',
+    'superseded_by',
+    'uuid',
+    'visibility',
+)
 
 register(
-    field_name='responsible_parties',
+    'responsible_parties',
     field_type='ManyToOneRel',
     report_block_class='reports.blocks.action_content.ActionResponsiblePartyReportFieldBlock',
     report_formatter_class='reports.report_formatters.ActionResponsiblePartyReportFieldFormatter',
     details_block_class='actions.blocks.action_content.ActionResponsiblePartiesBlock',
-    # force default
-    # dashboard_column_block_class='actions.blocks.action_dashboard.ResponsiblePartiesColumnBlock',
 )
-
 register(
-    field_name='tasks',
+    'tasks',
     field_type='ManyToOneRel',
     report_formatter_class='reports.report_formatters.ActionTasksFormatter',
     dashboard_column_block_class='actions.blocks.action_dashboard.TasksColumnBlock',
 )
-
 register(
-    field_name='categories',
+    'categories',
     field_type='ManyToManyField',
     report_block_class='reports.blocks.action_content.ActionCategoryReportFieldBlock',
     report_formatter_class='reports.report_formatters.ActionCategoryReportFieldFormatter',
     has_dashboard_column_block=False,
     details_block_class='actions.blocks.action_content.ActionOfficialNameBlock',
 )
-
 register(
-    field_name='dependencies',
+    'dependencies',
     field_type='Custom',
     has_report_block=False,
     has_dashboard_column_block=False,
     has_details_block=True,
     custom_label=_('Action dependencies'),
 )
-
 register(
-    field_name='attribute',
+    'attribute',
     field_type='Custom',
     has_report_block=True,
     has_dashboard_column_block=True,
@@ -77,23 +92,94 @@ register(
     report_formatter_class='reports.report_formatters.ActionAttributeTypeReportFieldFormatter',
     dashboard_column_block_class='actions.blocks.action_dashboard.FieldColumnBlock',
 )
-
 register(
-    field_name='merged_actions',
+    'merged_actions',
     custom_label=_('Merged actions'),
     has_dashboard_column_block=False,
+    has_report_block=False,
 )
-
 register(
-    field_name='official_name',
+    'official_name',
     details_block_class='actions.blocks.action_content.ActionOfficialNameBlock',
     has_dashboard_column_block=False,
+    has_report_block=False,
 )
-
 register(
-    field_name='primary_org',
+    'primary_org',
     has_dashboard_column_block=True,
     dashboard_column_block_class_name='OrganizationColumnBlock',
+    has_details_block=False,
+    has_report_block=False,
+)
+register(
+    'related_indicators',
+    dashboard_column_block_class_name='InidicatorsColumnBlock',
+    has_report_block=False,
+)
+register(
+    'contact_persons',
+    has_dashboard_column_block=False,
+    has_report_block=False,
+)
+register(
+    'lead_paragraph',
+    has_dashboard_column_block=False,
+    has_report_block=False,
+)
+register(
+    'links',
+    has_dashboard_column_block=False,
+    has_report_block=False,
+)
+register(
+    'related_actions',
+    has_dashboard_column_block=False,
+    has_report_block=False,
+)
+register(
+    'schedule',
+    has_dashboard_column_block=False,
+    has_report_block=False,
+)
+register(
+    'start_date',
+    has_details_block=False,
+    has_report_block=False,
+)
+register(
+    'end_date',
+    has_details_block=False,
+    has_report_block=False,
+)
+register(
+    'identifier',
+    has_details_block=False,
+    has_report_block=False,
+)
+register(
+    'implementation_phase',
+    has_details_block=False,
+)
+register(
+    'name',
+    has_details_block=False,
+    has_report_block=False,
+)
+register(
+    'manual_status_reason',
+    has_details_block=False,
+    has_dashboard_column_block=False,
+)
+register(
+    'status',
+    has_details_block=False,
+)
+register(
+    'updated_at',
+    has_details_block=False,
+    has_report_block=False,
 )
 
+
+action_registry.update_with_defaults()
 is_valid = action_registry.is_valid()
