927d90ec33b9caab44ae13913067b7614d926dd3
Timo Tuominen <tituomin@gmail.com>
2024-11-11 16:00:29 +0200

    Complete block implementations for details + reports

--- actions/action_fields.py
+++ actions/action_fields.py
@@ -1,50 +1,89 @@
+"""
+Specify the block configurations for all the action fields.
+
+This module only needs to contain custom configurations for actions fields which
+need custom classes and/or parameters to be used in the blocks. All of the default
+field block classes are automatically generated based on model introspection.
+
+We generate default implementations or configure custom implementations
+for blocks to be used in:
+    - The action details page
+    - The action dashboard table columns
+    - The action excel report types
+
+These share as much of the implementation as possible with each other.
+"""
 from __future__ import annotations
 
-from aplans.field_registry import ModelFieldProperties as Properties, ModelFieldRegistry
+from django.utils.translation import gettext_lazy as _
+
+from aplans.field_registry import ModelFieldProperties, ModelFieldRegistry
 
 from .models.action import Action
 
 action_registry = ModelFieldRegistry(Action)
 
-field = Properties
+def register(**kwargs):
+    action_registry.register(
+        ModelFieldProperties(**kwargs, model=Action),
+    )
+    action_registry.update_with_defaults()
 
-action_registry.register_all(
-    field(
-        field_name='responsible_parties',
-        field_type='ManyToOneRel',
-        report_block_class='reports.blocks.action_content.ActionResponsiblePartyReportFieldBlock',
-        report_formatter_class='reports.report_formatters.ActionResponsiblePartyReportFieldFormatter',
-        # force default
-        # dashboard_column_block_class='actions.blocks.action_dashboard.ResponsiblePartiesColumnBlock',
-    ),
-    field(
-        field_name='tasks',
-        field_type='ManyToOneRel',
-        report_formatter_class='reports.report_formatters.ActionTasksFormatter',
-        dashboard_column_block_class='actions.blocks.action_dashboard.TasksColumnBlock',
-    ),
-    field(
-        field_name='categories',
-        field_type='ManyToManyField',
-        report_block_class='reports.blocks.action_content.ActionCategoryReportFieldBlock',
-        report_formatter_class='reports.report_formatters.ActionCategoryReportFieldFormatter',
-        has_dashboard_column_block=False,
-    ),
-    field(
-        field_name='dependencies',
-        field_type='Custom',
-        has_report_block=False,
-        has_dashboard_column_block=False,
-        has_details_block=True,
-    ),
-    field(
-        field_name='attribute',
-        field_type='Custom',
-        has_report_block=True,
-        has_dashboard_column_block=False,
-        has_details_block=True,
-        details_block_class='actions.blocks.action_content.ActionContentAttributeTypeBlock',
-        report_block_class='reports.blocks.action_content.ActionAttributeTypeReportFieldBlock',
-        report_formatter_class='reports.report_formatters.ActionAttributeTypeReportFieldFormatter',
-    ),
+
+register(
+    field_name='responsible_parties',
+    field_type='ManyToOneRel',
+    report_block_class='reports.blocks.action_content.ActionResponsiblePartyReportFieldBlock',
+    report_formatter_class='reports.report_formatters.ActionResponsiblePartyReportFieldFormatter',
+    details_block_class='actions.blocks.action_content.ActionResponsiblePartiesBlock',
+    # force default
+    # dashboard_column_block_class='actions.blocks.action_dashboard.ResponsiblePartiesColumnBlock',
 )
+
+register(
+    field_name='tasks',
+    field_type='ManyToOneRel',
+    report_formatter_class='reports.report_formatters.ActionTasksFormatter',
+    dashboard_column_block_class='actions.blocks.action_dashboard.TasksColumnBlock',
+)
+
+register(
+    field_name='categories',
+    field_type='ManyToManyField',
+    report_block_class='reports.blocks.action_content.ActionCategoryReportFieldBlock',
+    report_formatter_class='reports.report_formatters.ActionCategoryReportFieldFormatter',
+    has_dashboard_column_block=False,
+    details_block_class='actions.blocks.action_content.ActionOfficialNameBlock',
+)
+
+register(
+    field_name='dependencies',
+    field_type='Custom',
+    has_report_block=False,
+    has_dashboard_column_block=False,
+    has_details_block=True,
+    custom_label=_('Action dependencies'),
+)
+
+register(
+    field_name='attribute',
+    field_type='Custom',
+    has_report_block=True,
+    has_dashboard_column_block=False,
+    has_details_block=True,
+    details_block_class='actions.blocks.action_content.ActionContentAttributeTypeBlock',
+    report_block_class='reports.blocks.action_content.ActionAttributeTypeReportFieldBlock',
+    report_formatter_class='reports.report_formatters.ActionAttributeTypeReportFieldFormatter',
+)
+
+register(
+    field_name='merged_actions',
+    custom_label=_('Merged actions'),
+)
+
+register(
+    field_name='official_name',
+    details_block_class='actions.blocks.action_content.ActionOfficialNameBlock',
+)
+
+is_valid = action_registry.is_valid()
