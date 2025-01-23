c2ef9393305f20d477cacb24bd265a4ab63eb96d
Bernhard Bliem <bernhard@bliem.xyz>
2025-01-23 16:43:04 +0100

    Move email settings to kausal-backend-common

--- aplans/settings.py
+++ aplans/settings.py
@@ -14,6 +14,7 @@ from celery.schedules import crontab
 from corsheaders.defaults import default_headers as default_cors_headers
 from environ.environ import ImproperlyConfigured
 
+from kausal_common import ENV_SCHEMA as COMMON_ENV_SCHEMA, register_settings as register_common_settings
 from kausal_common.deployment import set_secret_file_vars
 from kausal_common.sentry.init import init_sentry
 from kausal_common.storage import storage_settings_from_s3_url
@@ -45,10 +46,6 @@ env = environ.FileAwareEnv(
     STATIC_URL=(str, '/static/'),
     SENTRY_DSN=(str, ''),
     COOKIE_PREFIX=(str, PROJECT_NAME),
-    ALLOWED_SENDER_EMAILS=(list, []),
-    SERVER_EMAIL=(str, ''),
-    DEFAULT_FROM_EMAIL=(str, 'noreply@mj.kausal.tech'),
-    DEFAULT_FROM_NAME=(str, 'Kausal'),
     INTERNAL_IPS=(list, []),
     OIDC_ISSUER_URL=(str, ''),
     OIDC_CLIENT_ID=(str, ''),
@@ -63,12 +60,6 @@ env = environ.FileAwareEnv(
     ADFS_CLIENT_ID=(str, ''),
     ADFS_CLIENT_SECRET=(str, ''),
     ADFS_API_URL=(str, ''),
-    MAILGUN_API_KEY=(str, ''),
-    MAILGUN_SENDER_DOMAIN=(str, ''),
-    MAILGUN_REGION=(str, ''),
-    MAILJET_API_KEY=(str, ''),
-    MAILJET_SECRET_KEY=(str, ''),
-    SENDGRID_API_KEY=(str, ''),
     HOSTNAME_PLAN_DOMAINS=(list, ['localhost']),
     ELASTICSEARCH_URL=(str, ''),
     GOOGLE_MAPS_V3_APIKEY=(str, ''),
@@ -97,6 +88,7 @@ env = environ.FileAwareEnv(
     MOUNTED_SECRET_PATHS=(list, []),
     ENABLE_DEBUG_TOOLBAR=(bool, False),
     KAUSAL_PATHS_URL=(str, ''),
+    **COMMON_ENV_SCHEMA,
 )
 
 BASE_DIR = root()
@@ -151,15 +143,6 @@ ELASTICSEARCH_URL = env('ELASTICSEARCH_URL')
 
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_SENDER_EMAILS = env('ALLOWED_SENDER_EMAILS')
-SERVER_EMAIL = env('SERVER_EMAIL')
-if not SERVER_EMAIL and ALLOWED_SENDER_EMAILS:
-    SERVER_EMAIL = ALLOWED_SENDER_EMAILS[0]
-DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
-if not DEFAULT_FROM_EMAIL and ALLOWED_SENDER_EMAILS:
-    DEFAULT_FROM_EMAIL = ALLOWED_SENDER_EMAILS[0]
-DEFAULT_FROM_NAME = env('DEFAULT_FROM_NAME')
-
 ADMIN_BASE_URL = env('ADMIN_BASE_URL')
 WAGTAILADMIN_BASE_URL = ADMIN_BASE_URL
 WAGTAILADMIN_COMMENTS_ENABLED = True
@@ -198,7 +181,6 @@ INSTALLED_APPS = [
     'social_django',
     'django_extensions',
     'import_export',
-    'anymail',
     'modeltrans',
     'corsheaders',
 
@@ -548,31 +530,6 @@ SPECTACULAR_SETTINGS = {
         'OtherLanguagesEnum': LANGUAGES,
     },
 }
-
-#
-# Email
-#
-EMAIL_BACKEND = "anymail.backends.console.EmailBackend"
-ANYMAIL = {}
-
-if env.str('MAILGUN_API_KEY'):
-    EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
-    ANYMAIL['MAILGUN_API_KEY'] = env.str('MAILGUN_API_KEY')
-    ANYMAIL['MAILGUN_SENDER_DOMAIN'] = env.str('MAILGUN_SENDER_DOMAIN')
-    if env.str('MAILGUN_REGION'):
-        ANYMAIL['MAILGUN_API_URL'] = 'https://api.%s.mailgun.net/v3' % env.str('MAILGUN_REGION')
-
-if env.str('SENDGRID_API_KEY'):
-    EMAIL_BACKEND = "anymail.backends.sendgrid.EmailBackend"
-    ANYMAIL['SENDGRID_API_KEY'] = env.str('SENDGRID_API_KEY')
-
-if env.str('MAILJET_API_KEY'):
-    EMAIL_BACKEND = "anymail.backends.mailjet.EmailBackend"
-    ANYMAIL['MAILJET_API_KEY'] = env.str('MAILJET_API_KEY')
-    ANYMAIL['MAILJET_SECRET_KEY'] = env.str('MAILJET_SECRET_KEY')
-
-if DEBUG:
-    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # ckeditor for rich-text admin fields
 CKEDITOR_CONFIGS = {
@@ -961,6 +918,10 @@ WAGTAILADMIN_RICH_TEXT_EDITORS = {
 
 HIJACK_PERMISSION_CHECK = "admin_site.permissions.superusers_only_hijack"
 HIJACK_INSERT_BEFORE: str | None = None
+
+register_common_settings(locals())
+# Put type hints for stuff registered in register_common_settings here because mypy doesn't figure it out
+DEFAULT_FROM_NAME: str
 
 if importlib.util.find_spec('kausal_watch_extensions') is not None:
     INSTALLED_APPS.append('kausal_watch_extensions')
