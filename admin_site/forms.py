21b6d7e495bed42590cd3dafacd7a004af29a6f8
Timo Tuominen <tituomin@gmail.com>
2025-08-15 16:42:37 +0300

    Start using AdminModelForm base class from kausal_common

--- admin_site/forms.py
+++ admin_site/forms.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, override
 
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
@@ -8,9 +8,9 @@ from django.utils.translation import gettext_lazy
 from modeltrans.conf import get_available_languages
 from modeltrans.translator import get_i18n_field
 from modeltrans.utils import build_localized_fieldname
-from wagtail.admin.forms import WagtailAdminModelForm
 
 from kausal_common.i18n.helpers import convert_language_code, get_language_from_default_language_field
+from kausal_common.i18n.forms import LanguageAwareAdminModelForm
 
 if TYPE_CHECKING:
     from django.db.models import Model
@@ -41,66 +41,21 @@ class LoginForm(AuthenticationForm):
         return self.cleaned_data['username'].lower()
 
 
-class WatchAdminModelForm[ModelT: Model](WagtailAdminModelForm[ModelT]):
+class WatchAdminModelForm[ModelT: Model](LanguageAwareAdminModelForm[ModelT]):
     plan: Plan | None = None
-    realm_initialized: bool
 
-    def get_languages_to_show(self, i18n_field: TranslationField) -> set[str]:
-        """
-        Return a list of languages we want to display translation fields for.
-
-        This includes other languages of the plan, but also the
-        primary language of the plan, if it's different from the
-        primary language of the current model being edited.
-
-        It does not include the original language field without
-        the language suffix, since that field is added to the
-        form separately.
-
-        Please note: it is not enough nor necessary to hide the
-        language variant panels with is_shown since we have to
-        remove the form fields here anyway in order for the
-        form to be validated correctly.
-        """
-
-        original_field_language = self.get_primary_realm_language()
-        languages_to_show: set[str] = self.get_all_realm_languages()
-
-        if i18n_field.default_language_field:
-            original_field_language = get_language_from_default_language_field(self.instance, i18n_field)
-            original_field_language = convert_language_code(original_field_language, 'django')
-
-        # In the end, we make sure the modeltrans original field -- ie. the field
-        # without the language suffix which is saved directly to the original db
-        # field and not in the i18n field -- is never shown twice (once here and once as the
-        # PrimaryLanguagePanel which was added as a separate panel.
-        languages_to_show.remove(original_field_language)
-        return languages_to_show
-
+    @override
     def get_primary_realm_language(self) -> str:
         if self.plan is None:
             raise ValueError('Cannot get plan languages without plan.')
         return convert_language_code(self.plan.primary_language, 'django')
 
+    @override
     def get_all_realm_languages(self) -> set[str]:
         if self.plan is None:
             raise ValueError('Cannot get plan languages without plan.')
         result = set(self.plan.other_languages).union({ self.plan.primary_language })
         return {convert_language_code(lang, 'django') for lang in result}
-
-    def prune_i18n_fields(self):
-        i18n_field: TranslationField | None = get_i18n_field(self._meta.model)
-        if not i18n_field:
-            return
-        languages_to_show = self.get_languages_to_show(i18n_field)
-        if not languages_to_show:
-            return
-        for base_field_name in i18n_field.fields:
-            langs = list(get_available_languages(include_default=True))
-            for lang in langs:
-                fn = build_localized_fieldname(base_field_name, lang)
-                if lang not in languages_to_show and fn in self.fields:
-                    del self.fields[fn]
 
     def save(self, commit=True):
         obj = super().save(commit)
@@ -110,5 +65,3 @@ class WatchAdminModelForm[ModelT: Model](WagtailAdminModelForm[ModelT]):
         self.plan = kwargs.pop("plan", None)
         self.realm_initialized = (self.plan is not None)
         super().__init__(*args, **kwargs)
-        if self.plan:
-            self.prune_i18n_fields()
