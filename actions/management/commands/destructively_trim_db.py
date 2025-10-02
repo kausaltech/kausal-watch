8619baa4475cce155a3e123f01c7a4ad9d766703
Bernhard Bliem <bernhard@bliem.xyz>
2025-10-02 14:13:12 +0200

    Add option to exclude clients from deletion

--- actions/management/commands/destructively_trim_db.py
+++ actions/management/commands/destructively_trim_db.py
@@ -10,7 +10,6 @@ from wagtail.models import ModelLogEntry, PageLogEntry, Revision as WagtailRevis
 from actions.models.plan import Plan
 from admin_site.models import Client
 from orgs.models import Organization
-from pages.models import PlanRootPage
 from request_log.models import LoggedRequest
 from users.models import User
 
@@ -30,6 +29,12 @@ class Command(BaseCommand):
             metavar='UUID',
             action='append',
             help="Exclude the organization with the specified UUID from deletion",
+        )
+        parser.add_argument(
+            '--exclude-client',
+            metavar='ID',
+            action='append',
+            help="Exclude the client with the specified ID (primary key) from deletion",
         )
         parser.add_argument(
             '--no-confirm',
@@ -53,6 +58,8 @@ class Command(BaseCommand):
                 "Sorry, for preventing accidents, this management command only works if DEBUG is true and "
                 "DEPLOYMENT_TYPE is 'production'.",
             )
+
+        # Determine plans to delete
         all_identifiers = Plan.objects.values_list('identifier', flat=True)
         if not options.get('exclude_plan'):
             options['exclude_plan'] = []
@@ -62,6 +69,12 @@ class Command(BaseCommand):
         plans_to_delete = Plan.objects.qs.exclude(identifier__in=options['exclude_plan'])
         plans_to_keep = Plan.objects.qs.exclude(id__in=plans_to_delete)
         delete_identifiers = plans_to_delete.values_list('identifier', flat=True)
+        if options['exclude_plan']:
+            self.stdout.write(f"The following plans will not be deleted: {', '.join(options['exclude_plan'])}")
+        if delete_identifiers:
+            self.stdout.write(f"The following plans will be deleted with all related data: {', '.join(delete_identifiers)}")
+
+        # Determine organizations to delete
         orgs_to_keep = Organization.objects.qs.available_for_plans(plans_to_keep)
         if options['exclude_organization']:
             orgs_to_keep |= Organization.objects.filter(uuid__in=options['exclude_organization'])
@@ -70,10 +83,6 @@ class Command(BaseCommand):
         for org in orgs_to_delete.filter(depth=1):
             # Unnecessarily inefficient, but what the hell...
             num_delete_suborgs[org] = orgs_to_delete.filter(id__in=org.get_descendants()).count()
-        if options['exclude_plan']:
-            self.stdout.write(f"The following plans will not be deleted: {', '.join(options['exclude_plan'])}")
-        if delete_identifiers:
-            self.stdout.write(f"The following plans will be deleted with all related data: {', '.join(delete_identifiers)}")
         if num_delete_suborgs:
             strings = []
             for org, n in num_delete_suborgs.items():
@@ -84,9 +93,14 @@ class Command(BaseCommand):
                     string += f' (and {n} suborganizations)'
                 strings.append(string)
             self.stdout.write(f"The following organizations will be deleted: {', '.join(strings)}")
+
         self.stdout.write("Moreover, the following data will be deleted:")
         self.stdout.write("- all User instances that don't have a corresponding Person anymore")
-        self.stdout.write("- all Client instances that don't have a corresponding Plan anymore")
+        client_message = "- all Client instances that don't have a corresponding Plan anymore"
+        if options['exclude_client']:
+            client_names = Client.objects.filter(id__in=options['exclude_client']).values_list('name', flat=True)
+            client_message += f" and are not among the following: {', '.join(client_names)}"
+        self.stdout.write(client_message)
         self.stdout.write("- all Reversion Revision instances that don't have a corresponding User anymore")
         self.stdout.write("- all Wagtail Revision instances that don't have a corresponding User anymore")
         self.stdout.write("- all Wagtail ModelLogEntry instances that don't have a corresponding User anymore")
@@ -100,14 +114,23 @@ class Command(BaseCommand):
             if confirmation != 'y':
                 self.stdout.write(self.style.WARNING("Aborted by user."))
                 return
-        self.delete_data(plans_to_delete, orgs_to_delete, options['keep_page_log'], options['keep_model_log'])
+        self.delete_data(
+            plans_to_delete, orgs_to_delete, options['exclude_client'], options['keep_page_log'],
+            options['keep_model_log'],
+        )
         self.stdout.write(
             "You may also want to run the management command `wagtail_update_image_renditions --purge-only` to remove "
             "all renditions in the database."
         )
 
     @transaction.atomic
-    def delete_data(self, plans_to_delete, orgs_to_delete, keep_page_log: bool = False, keep_model_log: bool = False):
+    def delete_data(
+        self, plans_to_delete, orgs_to_delete, clients_to_keep: list[int] | None = None, keep_page_log: bool = False,
+        keep_model_log: bool = False,
+    ):
+        if clients_to_keep is None:
+            clients_to_keep = []
+
         # Delete plans
         # Iterate over plans and call `delete()` individually because bulk deletion would not call `delete()` and leave
         # related objects in place.
@@ -122,8 +145,8 @@ class Command(BaseCommand):
         # Delete users without persons
         _, by_type = User.objects.filter(person__isnull=True).delete()
         self.print_deleted_instances_by_model(by_type)
-        # Delete clients without plans
-        _, by_type = Client.objects.filter(plans__isnull=True).delete()
+        # Delete clients without plans unless excluded
+        _, by_type = Client.objects.filter(plans__isnull=True).exclude(id__in=clients_to_keep).delete()
         self.print_deleted_instances_by_model(by_type)
         # Delete Reversion revisions without users
         _, by_type = ReversionRevision.objects.filter(user__isnull=True).delete()
