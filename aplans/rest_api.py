076c2f154fad2b3390ba9d15570bd8aa6ce009a6
Timo Tuominen <tituomin@gmail.com>
2025-12-15 17:58:56 +0200

    Reformat to reduce nesting

--- aplans/rest_api.py
+++ aplans/rest_api.py
@@ -19,21 +19,29 @@ class PlanRelatedModelSerializer[Model: PlanRelatedModel](serializers.ModelSeria
 
     def __init__(self, *args, **kwargs):
         self.plan = kwargs.pop('plan', None)
-        if not self.plan:
-            context = kwargs.get('context')
-            if context is not None:
-                view = context['view']
-                if getattr(view, 'swagger_fake_view', False):
-                    # Called during schema generation
-                    assert 'plan_pk' not in view.kwargs
-                    self.plan = get_default_plan()
-                else:
-                    plan_pk = view.kwargs['plan_pk']
-                    plan = Plan.objects.filter(pk=plan_pk).prefetch_related('category_types').first()
-                    if plan is None:
-                        raise exceptions.NotFound('Plan not found')
-                    self.plan = plan
-            else:
-                # Probably called during schema generation
-                self.plan = get_default_plan()
+        if self.plan:
+            super().__init__(*args, **kwargs)
+            return
+
+        context = kwargs.get('context')
+        if context is None:
+            self.plan = get_default_plan()
+            super().__init__(*args, **kwargs)
+            return
+
+        view = context['view']
+
+        if getattr(view, 'swagger_fake_view', False):
+            # Called during schema generation
+            assert 'plan_pk' not in view.kwargs
+            self.plan = get_default_plan()
+            super().__init__(*args, **kwargs)
+            return
+
+        plan_pk = view.kwargs['plan_pk']
+        plan = Plan.objects.filter(pk=plan_pk).prefetch_related('category_types').first()
+        if plan is None:
+            raise exceptions.NotFound('Plan not found')
+
+        self.plan = plan
         super().__init__(*args, **kwargs)
