4d6d89208fd9adaf8e7b4c8e2b132755574dd6c4
Matias Wargelin <matias.wargelin@kausal.tech>
2026-01-05 16:19:05 +0200

    Add e2e test for filtering actions

--- e2e-tests/tests/actions.spec.ts
+++ e2e-tests/tests/actions.spec.ts
@@ -3,11 +3,24 @@ import {
   test,
 } from '@playwright/test';
 
+const listActionsPath = '/admin/actions/action/';
+
 test.describe('Test admin', () => {
   test('List actions', async ({ page }) => {
     await page.goto('/admin/');
     await page.getByRole('link', { name: 'Actions', exact: true }).click();
     await expect(page.getByLabel('Filter actions')).toBeVisible();
+  });
+
+  test('Filter actions by name', async ({ page }) => {
+    await page.goto(listActionsPath);
+    await expect(page.getByText('Test action 1')).toBeVisible();
+    await expect(page.getByText('Test action 2')).toBeVisible();
+
+    await expect(page.getByRole('textbox', { name: 'Filter actions' })).toBeVisible();
+    await page.getByRole('textbox', { name: 'Filter actions' }).fill('Test action 1');
+    await expect(page.getByText('Test action 1')).toBeVisible();
+    await expect(page.getByText('Test action 2')).toBeHidden();
   });
 })
 
