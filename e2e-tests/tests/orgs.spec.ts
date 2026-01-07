c363542a23630e0ac4f8d105f9ae53b9aa3ab3c9
Matias Wargelin <matias.wargelin@kausal.tech>
2026-01-07 09:00:33 +0200

    Refactor organizations e2e tests

--- e2e-tests/tests/orgs.spec.ts
+++ e2e-tests/tests/orgs.spec.ts
@@ -3,24 +3,25 @@ import {
   test,
 } from '@playwright/test';
 
-test.describe('Test organization admin', () => {
-  const organizationsPath = '/admin/snippets/orgs/organization/';
+const listOrganizationsPath = '/admin/snippets/orgs/organization/';
+const editOrganizationPath = RegExp(`${listOrganizationsPath}edit/[0-9]+/`);
 
+test.describe('Test organization admin', () => {
   test('Organizations appear in menu', async ({ page }) => {
     await page.goto('/admin/');
     const link = await page.getByRole('link', { name: 'Organizations' }).getAttribute('href');
-    expect(link).toBe(organizationsPath);
+    expect(link).toBe(listOrganizationsPath);
   });
 
   test('List organizations', async ({ page }) => {
-    await page.goto(organizationsPath);
+    await page.goto(listOrganizationsPath);
     await expect(page.getByRole('table')).toContainText('Kausal Oy');
   });
 
   test('Organization edit page has title', async ({ page }) => {
-    await page.goto(organizationsPath);
+    await page.goto(listOrganizationsPath);
     await page.getByRole('link', { name: 'Kausal Oy' }).click();
-    await page.waitForURL(RegExp(`${organizationsPath}edit/[0-9]+/`));
+    await page.waitForURL(editOrganizationPath);
     await expect(page.locator('#header-title')).toContainText('Kausal Oy');
   });
 })
