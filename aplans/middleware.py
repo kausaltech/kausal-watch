dbc74dcf86de482cc539dc2d87cee86defb0484c
Timo Tuominen <tituomin@gmail.com>
2026-01-29 15:06:13 +0200

    Fix import
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- aplans/middleware.py
+++ aplans/middleware.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import json
 import re
-from collections.abc import Coroutine, Mapping
+from collections.abc import Mapping
 from typing import Any
 
 from django import http
