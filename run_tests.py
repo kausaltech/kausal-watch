9cd0f96998b732962f52738b3ce16234834f0ffe
Juha Yrjölä <juha.yrjola@kausal.tech>
2026-02-20 11:39:58 +0200

    pytest: limit number of test failures to 20

--- run_tests.py
+++ run_tests.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-import importlib
+from __future__ import annotations
+
 import sys
+from importlib.util import find_spec
 
 import pytest
 
 EXTENSIONS_PACKAGE_NAME = 'kausal_watch_extensions'
 
 if __name__ == '__main__':
-    args = ['--pyargs', *sys.argv[1:], '.']
-    if importlib.util.find_spec(EXTENSIONS_PACKAGE_NAME) is not None:
+    args = ['--maxfail', '20', '--pyargs', *sys.argv[1:], '.']
+    if find_spec(EXTENSIONS_PACKAGE_NAME) is not None:
         args.append(EXTENSIONS_PACKAGE_NAME)
     raise SystemExit(pytest.main(args))
