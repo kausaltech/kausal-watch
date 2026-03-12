d33807d644f883f4322f69ba248dab7e6ff49f26
Bernhard Bliem <bernhard@bliem.xyz>
2026-03-12 13:13:42 +0100

    Migrate datasets GraphQL API to Strawberry

--- datasets/schema.py
+++ datasets/schema.py
@@ -1,6 +1,11 @@
 from __future__ import annotations
 
-import graphene
+from typing import TYPE_CHECKING, Annotated, cast
+
+import strawberry
+import strawberry_django
+
+from grapple.registry import registry as grapple_registry
 
 from kausal_common.datasets.models import (
     DataPoint,
@@ -13,106 +18,123 @@ from kausal_common.datasets.models import (
     DimensionCategory,
     DimensionScope,
 )
-from kausal_common.datasets.schema import (
-    DataPointNode as BaseDataPointNode,
-    DatasetMetricNode as BaseDatasetMetricNode,
-    DatasetNode as BaseDatasetNode,
-    DatasetSchemaDimensionNode as BaseDatasetSchemaDimensionNode,
-    DatasetSchemaNode as BaseDatasetSchemaNode,
-    DatasetSchemaScopeNode as BaseDatasetSchemaScopeNode,
-    DatasetSchemaScopeTypeNode as BaseDatasetSchemaScopeTypeNode,
-    DatasetScopeTypeNode as BaseDatasetScopeTypeNode,
-    DimensionCategoryNode as BaseDimensionCategoryNode,
-    DimensionNode as BaseDimensionNode,
-    DimensionScopeNode as BaseDimensionScopeNode,
-    DimensionScopeTypeNode as BaseDimensionScopeTypeNode,
-)
-
-from aplans.graphql_types import DjangoNode, register_django_node
+from kausal_common.strawberry.registry import register_strawberry_type
 
 from actions.schema import ActionNode, CategoryNode, CategoryTypeNode, PlanNode
 from indicators.schema import IndicatorNode
 
+if TYPE_CHECKING:
+    from strawberry import auto
 
-class DimensionNode(BaseDimensionNode, DjangoNode[Dimension]):
-    class Meta:
-        model = Dimension
-        name = 'DatasetsDimension'  # clashes otherwise with type name in indicators.schema
-        fields = ('uuid', 'name', 'categories', 'scopes')
+DimensionScopeType = Annotated[
+    PlanNode | CategoryTypeNode,
+    strawberry.union('DimensionScopeType'),
+]
 
+DatasetSchemaScopeType = Annotated[
+    PlanNode | CategoryTypeNode,
+    strawberry.union('DatasetSchemaScopeType'),
+]
 
-class DimensionCategoryNode(BaseDimensionCategoryNode, DjangoNode[DimensionCategory]):
-    class Meta:
-        model = DimensionCategory
-        name = 'DatasetsDimensionCategory'  # clashes otherwise with type name in indicators.schema
-        fields = ('uuid', 'dimension', 'label')
-
-
-class DimensionScopeNode(BaseDimensionScopeNode, DjangoNode[DimensionScope]):
-    scope = graphene.Field(lambda: DimensionScopeTypeNode)
-
-    class Meta:
-        model = DimensionScope
-        fields = '__all__'
+DatasetScopeType = Annotated[
+    ActionNode | CategoryNode,
+    strawberry.union('DatasetScopeType'),
+]
 
 
-class DimensionScopeTypeNode(BaseDimensionScopeTypeNode):
-    class Meta:
-        types = (PlanNode, CategoryTypeNode)
+@register_strawberry_type
+@strawberry_django.type(Dimension, name='DatasetsDimension')
+class DimensionNode:
+    uuid: auto
+    name: auto
+    categories: list[DimensionCategoryNode]
+    scopes: list[DimensionScopeNode]
 
 
-
-class DataPointNode(BaseDataPointNode, DjangoNode[DataPoint]):
-    class Meta:
-        model = DataPoint
-        fields = ('uuid', 'dataset', 'date', 'value', 'dimension_categories')
-
-
-class DatasetSchemaScopeNode(BaseDatasetSchemaScopeNode, DjangoNode[DatasetSchemaScope]):
-    scope = graphene.Field(lambda: DatasetSchemaScopeTypeNode)
-
-    class Meta:
-        model = DatasetSchemaScope
-        fields = '__all__'
+@register_strawberry_type
+@strawberry_django.type(DimensionCategory, name='DatasetsDimensionCategory')
+class DimensionCategoryNode:
+    uuid: auto
+    dimension: DimensionNode
+    label: auto
 
 
-class DatasetSchemaScopeTypeNode(BaseDatasetSchemaScopeTypeNode):
-    class Meta:
-        types = (
-            PlanNode, CategoryTypeNode,
-        )
+@register_strawberry_type
+@strawberry_django.type(DimensionScope, name='DimensionScope')
+class DimensionScopeNode:
+    @strawberry_django.field
+    def scope(self: DimensionScope) -> DimensionScopeType | None:
+        return cast('DimensionScopeType', self.scope)
 
 
-class DatasetScopeTypeNode(BaseDatasetScopeTypeNode):
-    class Meta:
-        types = (
-            ActionNode, CategoryNode, IndicatorNode,
-        )
+@register_strawberry_type
+@strawberry_django.type(DataPoint, name='DataPoint')
+class DataPointNode:
+    uuid: auto
+    dataset: DatasetNode
+    date: auto
+    dimension_categories: list[DimensionCategoryNode]
 
-class DatasetSchemaDimensionNode(BaseDatasetSchemaDimensionNode, DjangoNode[DatasetSchemaDimension]):
-    class Meta:
-        model = DatasetSchemaDimension
-        fields = ('order', 'dimension', 'schema')
-
-
-class DatasetMetricNode(BaseDatasetMetricNode):
-    class Meta:
-        model = DatasetMetric
-        fields = '__all__'
+    @strawberry_django.field
+    def value(self: DataPoint) -> float | None:
+        return float(self.value) if self.value is not None else None
 
 
-@register_django_node
-class DatasetSchemaNode(BaseDatasetSchemaNode, DjangoNode[DatasetSchema]):
-    class Meta:
-        model = DatasetSchema
-        fields = ('uuid', 'time_resolution', 'name', 'scopes', 'dimensions', 'metrics')
+@register_strawberry_type
+@strawberry_django.type(DatasetSchemaScope, name='DatasetSchemaScope')
+class DatasetSchemaScopeNode:
+    @strawberry_django.field
+    def scope(self: DatasetSchemaScope) -> DatasetSchemaScopeType | None:
+        return cast('DatasetSchemaScopeType', self.scope)
 
 
-class DatasetNode(BaseDatasetNode, DjangoNode[Dataset]):
-    scope = graphene.Field(lambda: DatasetScopeTypeNode)
-    class Meta:
-        model = Dataset
-        fields = ('uuid', 'schema', 'data_points')
+@register_strawberry_type
+@strawberry_django.type(DatasetSchemaDimension, name='DatasetSchemaDimension')
+class DatasetSchemaDimensionNode:
+    order: auto
+    dimension: DimensionNode
+    schema: DatasetSchemaNode
+
+
+@register_strawberry_type
+@strawberry_django.type(DatasetMetric, name='DatasetMetricNode')
+class DatasetMetricNode:
+    uuid: auto
+    schema: DatasetSchemaNode
+    name: auto
+    label: auto
+    unit: auto
+    order: auto
+
+
+@register_strawberry_type
+@strawberry_django.type(DatasetSchema, name='DatasetSchema')
+class DatasetSchemaNode:
+    uuid: auto
+    name: auto
+    scopes: list[DatasetSchemaScopeNode]
+    dimensions: list[DatasetSchemaDimensionNode]
+    metrics: list[DatasetMetricNode]
+
+    @strawberry_django.field
+    def time_resolution(self: DatasetSchema) -> str:
+        return self.time_resolution.upper()
+
+
+# Register in Grapple's django_models so PlanDatasetsBlock can find it
+grapple_registry.django_models[DatasetSchema] = DatasetSchemaNode
+
+
+@register_strawberry_type
+@strawberry_django.type(Dataset, name='Dataset')
+class DatasetNode:
+    uuid: auto
+    schema: DatasetSchemaNode | None
+    data_points: list[DataPointNode]
+
+    @strawberry_django.field
+    def scope(self: Dataset) -> DatasetScopeType | None:
+        return cast('DatasetScopeType', self.scope)
 
 
 class Query:
