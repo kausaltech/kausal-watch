47774a3fc69a098708ac1754fbd6d3d4abaee2b4
Timo Tuominen <tituomin@gmail.com>
2026-04-01 15:58:57 +0300

    Introduce yet another type error

--- actions/models/plan.py
+++ actions/models/plan.py
@@ -155,7 +155,7 @@ class PlanQuerySet(PermissionedQuerySet['Plan'], MultilingualQuerySet['Plan']):
         # FIXME: Add indicators
         return self.filter(id__in=staff_actions)
 
-    def visible_for_user(self, user: str) -> PlanQuerySet:
+    def visible_for_user(self, user: str) -> Self:
         """Filter by visibility using permission policy."""
         if user is None:
             user = AnonymousUser()
