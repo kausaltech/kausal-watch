822a3a1b650268eca0525a98c107c083b9dfbc17
Timo Tuominen <tituomin@gmail.com>
2026-06-29 18:32:14 +0300

    Change plan defaults wrt public site authentication
    For new plans, authentication required by default
    to see the public site.

--- actions/models/features.py
+++ actions/models/features.py
@@ -39,13 +39,13 @@ class PlanFeatures(PlanRelatedModelWithRevision):
         help_text=_('Should the public website contain a link to the admin login?'),
     )
     allow_public_site_login = models.BooleanField(
-        default=False,
+        default=True,
         verbose_name=_('Allow logging in to the public website'),
         help_text=_('Should users be able to have authenticated sessions in the public UI?'),
     )
 
     expose_unpublished_plan_only_to_authenticated_user = models.BooleanField(
-        default=False,
+        default=True,
         verbose_name=_('Expose unpublished plan only to authenticated users'),
         help_text=_('Should the plan be exposed only to authenticated users if not published?'),
     )
