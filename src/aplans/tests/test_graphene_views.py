c7beb111e07a8208c35a1dbbd5b8e1494d707f98
Jan-Erik S. <6697396+Bhavatu@users.noreply.github.com>
2026-08-25 16:08:45 +0300

    Bypass graphene view cache for any non-query operation

---
+++ src/aplans/tests/test_graphene_views.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, cast
+
+from django.test import RequestFactory
+
+from aplans.graphene_views import SentryGraphQLView
+
+if TYPE_CHECKING:
+    from aplans.types import WatchAPIRequest
+
+
+def test_get_cache_key_returns_none_for_mutation():
+    view = SentryGraphQLView.__new__(SentryGraphQLView)
+    request = cast(
+        'WatchAPIRequest',
+        RequestFactory().post('/v1/graphql/', HTTP_X_CACHE_PLAN_IDENTIFIER='some-plan'),
+    )
+    assert view.get_cache_key(request, None, 'mutation { foo }', {}, None) is None
