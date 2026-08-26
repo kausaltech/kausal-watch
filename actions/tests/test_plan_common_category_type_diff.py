8bc0ac349e8928b1034b702b54061b8df359a35d
Juha Yrjölä <juha.yrjola@kausal.tech>
2026-08-26 19:36:05 +0000

    Test clearing plan common category types

--- actions/tests/test_plan_common_category_type_diff.py
+++ actions/tests/test_plan_common_category_type_diff.py
@@ -1,7 +1,13 @@
+from django.forms import modelform_factory
+from django.http import HttpResponse
+
 import pytest
 
-from actions.models.category import CommonCategoryType
-from admin_site.wagtail import diff_common_category_types
+from actions.models import Plan
+from actions.models.category import CategoryType, CommonCategoryType
+from actions.tests.factories import CategoryTypeFactory, CommonCategoryTypeFactory
+from actions.wagtail_admin import PlanAdmin, PlanEditView
+from admin_site.wagtail import AplansEditView, diff_common_category_types
 
 pytestmark = pytest.mark.django_db
 
@@ -34,3 +40,26 @@ def test_diff_reports_added_and_removed(plan, common_category_type_factory):
 
     assert added == [added_cct]
     assert removed == [removed_cct]
+
+
+def test_plan_edit_clears_common_category_types(plan, monkeypatch):
+    common_category_type = CommonCategoryTypeFactory.create()
+    category_type = CategoryTypeFactory.create(plan=plan, common=common_category_type)
+    plan.common_category_types.add(common_category_type)
+    form_class = modelform_factory(Plan, fields=('common_category_types',))
+    form = form_class(data={'common_category_types': []}, instance=plan)
+    assert form.is_valid()
+    expected_response = HttpResponse()
+
+    def save_form(_view, form):
+        form.save()
+        return expected_response
+
+    monkeypatch.setattr(AplansEditView, 'form_valid', save_form)
+    view = PlanEditView(PlanAdmin(), str(plan.pk))
+
+    response = view.form_valid(form)
+
+    assert response is expected_response
+    assert not plan.common_category_types.exists()
+    assert not CategoryType.objects.filter(pk=category_type.pk).exists()
