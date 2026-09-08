536b31033e3442056850f7e042456c690f44c6a4
Bernhard Bliem <bernhard@bliem.xyz>
2026-09-08 11:57:16 +0300

    Remove unused GraphQL execution context

--- src/aplans/graphql_views.py
+++ src/aplans/graphql_views.py
@@ -2,13 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, override
 
-from django.conf import settings
-from django.http import HttpRequest
-from graphql.execution import ExecutionContext
-
-from loguru import logger
-
-from kausal_common.deployment import env_bool
 from kausal_common.strawberry.views import GraphQLView, GraphQLWSConsumer, SyncGraphQLHTTPConsumer
 from kausal_common.users import user_or_none
 
@@ -17,46 +10,10 @@ from .schema_context import WatchGraphQLContext
 if TYPE_CHECKING:
     from django.http import HttpRequest
     from django.http.response import HttpResponse
-    from graphql import (
-        GraphQLOutputType,
-    )
-    from graphql.error import GraphQLError
-    from graphql.pyutils import Path
     from strawberry.channels import (
         ChannelsRequest,
     )
     from strawberry.http.temporal_response import TemporalResponse
-
-    from aplans.types import GQLPlanContext
-
-
-SUPPORTED_LANGUAGES = {x[0] for x in settings.LANGUAGES}
-
-
-logger = logger.bind(markup=True)
-
-GRAPHQL_CAPTURE_QUERIES = env_bool('GRAPHQL_CAPTURE_QUERIES', default=False)
-
-
-# FIXME: Not used anywhere; any code worth keeping?
-class WatchExecutionContext(ExecutionContext):
-    context_value: GQLPlanContext
-
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-
-    @override
-    def handle_field_error(
-        self,
-        error: GraphQLError,
-        return_type: GraphQLOutputType,
-        path: Path,
-    ) -> None:
-        if settings.DEBUG and error.original_error is not None and not getattr(error, '_was_printed', False):
-            exc = error.original_error
-            logger.opt(exception=exc).error('GraphQL field error at {path}', path=error.path)
-            setattr(error, '_was_printed', True)  # noqa: B010
-        return super().handle_field_error(error, return_type, path)
 
 
 class WatchGraphQLWSConsumer(GraphQLWSConsumer[WatchGraphQLContext]):
